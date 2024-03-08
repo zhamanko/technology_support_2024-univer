8cef9f8d47230a6c5b0a72713cd53ee02a560877
Sega-128 <seginigor96@gmail.com>
2024-03-08 08:56:16 +0200

    style: update prettier tabWidth , fix all

--- adapter/github.adapter.js
+++ adapter/github.adapter.js
@@ -6,64 +6,64 @@ const { logger } = require('../utils/logger.utils');
 const RETRY_STATUS_CODES = [403, 500, 503];
 
 const gotClient = got.extend({
-  prefixUrl: 'https://api.github.com/',
-  headers: {
-    accept: 'application/vnd.github+json',
-    'User-Agent': 'Awesome-Octocat-App-TOP-Contributor',
-    Authorization: `Bearer ${GITHUB_PERSONAL_TOKEN}`,
-  },
-  retry: {
-    limit: 3,
-    methods: ['GET', 'POST'],
-    statusCodes: RETRY_STATUS_CODES,
-    calculateDelay: (response) => {
-      if (RETRY_STATUS_CODES.includes(response.statusCode)) {
-        return 20000 * response.attemptCount;
-      } else {
-        return 0;
-      }
-    },
-  },
+   prefixUrl: 'https://api.github.com/',
+   headers: {
+      accept: 'application/vnd.github+json',
+      'User-Agent': 'Awesome-Octocat-App-TOP-Contributor',
+      Authorization: `Bearer ${GITHUB_PERSONAL_TOKEN}`,
+   },
+   retry: {
+      limit: 3,
+      methods: ['GET', 'POST'],
+      statusCodes: RETRY_STATUS_CODES,
+      calculateDelay: (response) => {
+         if (RETRY_STATUS_CODES.includes(response.statusCode)) {
+            return 20000 * response.attemptCount;
+         } else {
+            return 0;
+         }
+      },
+   },
 });
 
 class Github {
-  constructor() {
-    this.client = gotClient;
-  }
+   constructor() {
+      this.client = gotClient;
+   }
 
-  async getContributors({ owner, repo, page = 1, perPage = 100, type }) {
-    try {
-      const response = await this.client.get(`repos/${owner}/${repo}/contributors`, {
-        searchParams: { per_page: perPage, page, type },
-        method: 'GET',
-        responseType: 'json',
-      });
+   async getContributors({ owner, repo, page = 1, perPage = 100, type }) {
+      try {
+         const response = await this.client.get(`repos/${owner}/${repo}/contributors`, {
+            searchParams: { per_page: perPage, page, type },
+            method: 'GET',
+            responseType: 'json',
+         });
 
-      const data = response.body;
-      return data;
-    } catch (error) {
-      logger.error(error);
+         const data = response.body;
+         return data;
+      } catch (error) {
+         logger.error(error);
 
-      throw new Error('Service is temporarily unavailable, please try again later');
-    }
-  }
+         throw new Error('Service is temporarily unavailable, please try again later');
+      }
+   }
 
-  async postGraphQLQuery({ query }) {
-    try {
-      const response = await this.client.post('graphql', {
-        json: {
-          query,
-        },
-        responseType: 'json',
-      });
+   async postGraphQLQuery({ query }) {
+      try {
+         const response = await this.client.post('graphql', {
+            json: {
+               query,
+            },
+            responseType: 'json',
+         });
 
-      return response.body;
-    } catch (error) {
-      logger.error(error);
+         return response.body;
+      } catch (error) {
+         logger.error(error);
 
-      throw new Error('Service is temporarily unavailable, please try again later');
-    }
-  }
+         throw new Error('Service is temporarily unavailable, please try again later');
+      }
+   }
 }
 
 module.exports = { Github: new Github() };
--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const loggerHTTP = require('./utils/logger.utils');
 const app = express();
 
 const corsOptions = {
-  origin: URL_CLIENT,
-  credentials: true,
+   origin: URL_CLIENT,
+   credentials: true,
 };
 
 app.use(loggerHTTP);
@@ -18,5 +18,5 @@ app.use(cors(corsOptions));
 app.use('/', require('./routes/index'));
 
 app.listen(PORT, () => {
-  loggerHTTP.logger.info(`Start server port: ${PORT}`);
+   loggerHTTP.logger.info(`Start server port: ${PORT}`);
 });
