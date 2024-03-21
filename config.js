7316f0a9cc566c6d18e76800e720ef057727b9e3
Slavik Lenko <slavko.lenko28.06.2016@gmail.com>
2024-03-21 23:03:05 +0200

    Fix eslint errors file config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
  PORT: process.env.PORT || 3000,
  GITHUB_PERSONAL_TOKEN: process.env.GITHUB_PERSONAL_TOKEN,
- URL_CLIENT:  process.env.URL_CLIENT || 'http://localhost:8080',
+ URL_CLIENT: process.env.URL_CLIENT || 'http://localhost:8080',
 };
