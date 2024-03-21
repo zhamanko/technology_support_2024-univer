0df583fe438f747408bb0df105edcc84cf6d46e1
Slavik Lenko <slavko.lenko28.06.2016@gmail.com>
2024-03-21 23:09:03 +0200

    Fix eplint errors and fix path module 'express'

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+
 const router = express.Router();
 const { Github } = require('../controllers');
 
-router.get("/getTopRepositories", Github.getTopContributors);
+router.get('/getTopRepositories', Github.getTopContributors);
 
 module.exports = router;
