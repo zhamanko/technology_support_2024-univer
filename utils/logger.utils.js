f6a319b43cef6a1a1796db4b0f9f9f47968751ac
Slavik Lenko <slavko.lenko28.06.2016@gmail.com>
2024-03-21 23:22:57 +0200

    Fix eslint errors file utils/logger.utils.js

--- utils/logger.utils.js
+++ utils/logger.utils.js
@@ -1,24 +1,24 @@
-const path = require("node:path");
-const pino = require("pino-http");
+const path = require('node:path');
+const pino = require('pino-http');
 
-const isPrоd = process.env.NODE_ENV == "production";
+const isProd = process.env.NODE_ENV === 'production';
 
 const customLogLevel = (req, res, err) => {
  if (res.statusCode >= 400 && res.statusCode < 500) {
-  return "warn";
+  return 'warn';
  }
  if (res.statusCode >= 500 || err) {
-  return "error";
+  return 'error';
  }
  if (res.statusCode >= 300 && res.statusCode < 400) {
-  return "silent";
+  return 'silent';
  }
- return "info";
+ return 'info';
 };
 
 const devConfig = {
  transport: {
-  target: "pino-pretty",
+  target: 'pino-pretty',
  },
  serializers: {
   req: function asReqValue(req) {
@@ -38,8 +38,8 @@ const devConfig = {
  },
  customLogLevel,
  customSuccessMessage(req, res) {
-  if (res.statusCode == 404) {
-   return "resource not found";
+  if (res.statusCode === 404) {
+   return 'resource not found';
   }
   return `${req.method} completed`;
  },
@@ -52,15 +52,15 @@ const devConfig = {
 };
 
 const getFilePath = () => {
- const directoryPath = "./logs";
+ const directoryPath = './logs';
  const dateLog = new Date().toISOString().slice(0, 10);
- const filePath = path.join(directoryPath, "log-file-${dateLog}.log");
+ const filePath = path.join(directoryPath, `log-file-${dateLog}.log`);
  return filePath;
 };
 
 const prodLogger = {
  transport: {
-  target: "pino/file",
+  target: 'pino/file',
   options: { destination: getFilePath(), mkdir: isProd },
  },
 };
