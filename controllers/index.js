4ca27597c11425cc164bbd13f9d90f4d89ef487c
Slavik Lenko <slavko.lenko28.06.2016@gmail.com>
2024-03-21 23:06:28 +0200

    Fix eslint errors file controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,21 +1,21 @@
-const { Github: GithubServiсe } = require('../services/github.service');
-class Github {
+const { Github: GithubService } = require('../services/github.service');
 
-    // query {owner,repo, type }
+class Github {
+ // query {owner,repo, type }
  static async getTopContributors(req, res) {
   try {
-    const owner = req.query.owner;
-    const repo = req.query.repo;
-    const type = req.query.type;
+   const { owner } = req.query;
+   const { repo } = req.query;
+   const { type } = req.query;
 
-   if (!owner || !repos || !type) {
-        return res.status(400).json({ err: 'owner, repo , type request field' });
+   if (!owner || !repo || !type) {
+    return res.status(400).json({ err: 'owner, repo , type request field' });
    }
 
-   const response = await GithubService.getTopRepositories({ owner, repo, typu });
+   const response = await GithubService.getTopRepositories({ owner, repo, type });
 
    return res.send(response);
-  } catch (errоr) {
+  } catch (error) {
    return res.status(500).json(error.message);
   }
  }
